c136b7653df11e7b1a103bce34c30f0a17fa6e15
Jonathan Ismael Balbuena <jonathan.jib@hotmail.com>
2025-08-03 17:08:20 -0300

    Init Config

---
+++ src/main/java/com/jbalbuen/demoPaymentWS/DemoPaymentWsApplication.java
@@ -0,0 +1,13 @@
+package com.jbalbuen.demoPaymentWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPaymentWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPaymentWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbalbuen/demoPaymentWS/DemoPaymentWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbalbuen.demoPaymentWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPaymentWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
